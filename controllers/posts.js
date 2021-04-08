0e946bfa1b0adcb7574e0746144f1917fe153e8b
Elad Tal <eladtal14@gmail.com>
2021-04-08 11:16:21 +0300

    first commit

---
+++ controllers/posts.js
@@ -0,0 +1,56 @@
+
+import mongoose from 'mongoose';
+import express from 'express';
+import PostMessage from '../models/postMessage.js'
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find()
+    res.status(200).json(postMessages)
+  } catch (err) {
+    res.status(404).json({ message: err.message })
+  }
+}
+export const createPost = async (req, res) => {
+  const post = req.body
+  const newPost = new PostMessage(post)
+  try {
+    await newPost.save()
+    res.status(201).json(newPost)
+  } catch (err) {
+    res.status(409).json({ message: err.message + 'add not work' })
+  }
+}
+
+export const updatePost = async (req, res) => {
+  const post = req.body
+  const { id: _id } = req.params
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No Post to update with that Id')
+  try {
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true })
+    res.json(updatedPost)
+  } catch (err) {
+    res.status(409).json({ message: err.message })
+  }
+}
+export const deletePost = async (req, res) => {
+  const { id: _id } = req.params
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No Post to delete with that Id')
+  try {
+    await PostMessage.findByIdAndRemove(_id)
+    res.json({ message: 'post deleted secessfully' })
+  } catch (err) {
+    res.status(409).json({ message: err.message })
+  }
+}
+export const likePost = async (req, res) => {
+  const { id: _id } = req.params
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No Post to update likes with that Id')
+  try {
+    const post = await PostMessage.findById(_id)
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { likeCount: post.likeCount + 1 }, { new: true })
+    res.json(updatedPost)
+  } catch (err) {
+    res.status(409).json({ message: err.message })
+  }
+}
---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import postRoutes from './routes/posts.js'
+import path from 'path'
+const app = express()
+// app.use(express.static('public'));
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+// const PORT = process.env.PORT || 5000
+
+
+app.use('/posts', postRoutes)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+  const corsOptions = {
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    credentials: true
+  }
+  app.use(cors(corsOptions))
+}
+
+
+const CONNECTION_URL = 'mongodb+srv://elad:8dgCcE63AJei6cMe@cluster0.jspyg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => { })
+  .catch((err) => console.log(err.message))
+mongoose.set('useFindAndModify', false)
+
+app.get('/**', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.listen(process.env.PORT || 5000, () => console.log(`Server running on port: ${process.env.PORT || 5000}`))
