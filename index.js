8842cd55837d673ce1d99015832eecbe43c4dd07
Elad Tal <eladtal14@gmail.com>
2021-04-08 12:29:42 +0300

    test15

--- index.js
+++ index.js
@@ -32,8 +32,8 @@ mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: tr
   .catch((err) => console.log(err.message))
 mongoose.set('useFindAndModify', false)
 
-// app.get('/**', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'))
-// })
+app.get('/**', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 http1.listen(process.env.PORT || 5000, () => console.log(`Server running on port: ${process.env.PORT || 5000}`))
