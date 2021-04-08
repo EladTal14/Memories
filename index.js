773e77557a91653815a6675087af81a7dbf6ac81
Elad Tal <eladtal14@gmail.com>
2021-04-08 11:56:13 +0300

    test 7

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('public'))
+  app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
   const corsOptions = {
     origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
@@ -30,8 +30,8 @@ mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: tr
   .catch((err) => console.log(err.message))
 mongoose.set('useFindAndModify', false)
 
-// app.get('/**', (req, res) => {
-//   res.sendFile(path.join('public', 'index.html'))
-// })
+app.get('/**', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 app.listen(process.env.PORT || 5000, () => console.log(`Server running on port: ${process.env.PORT || 5000}`))
